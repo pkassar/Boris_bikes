c7cc0673a845f1f534fbcfa77e88ac3a1f0c6d53
Paul Kassar <your email>
2017-04-11 15:56:06 +0100

    second spec text

--- lib/DockingStation.rb
+++ lib/DockingStation.rb
@@ -1,4 +1,11 @@
 class DockingStation
   def release_bike
+    bike = Bike.new
+  end
+end
+
+
+class Bike
+  def working?
   end
 end
---
+++ spec/Bike_spec.rb
@@ -0,0 +1,5 @@
+require './lib/dockingstation.rb'
+
+describe Bike do
+  it {is_expected.to respond_to :working?}
+end
